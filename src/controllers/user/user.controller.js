cc44638f14583f59dfd9456f231ad415883f6025
YABF0801 <yabf0801@gmail.com>
2023-03-07 23:44:09 -0500

    fix arreglar controlador updateUser para actualizar usuario

--- src/controllers/user/user.controller.js
+++ src/controllers/user/user.controller.js
@@ -49,54 +49,49 @@ const FindSingleUser = async (req, res) => {
 };
 
 const UpdateUser = async (req, res) => {
-  try {
-    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-      return res.status(400).json({ message: 'El ID enviado no es válido' });
-    }
-    else{    
-    const { errors, isDataValid } = await userDataValidation(req.body);
-    if (!isDataValid) return res.status(400).json(errors);
+   //  const { errors, isDataValid } = await userDataValidation(req.body);
+   //  if (!isDataValid) return res.status(400).json(errors);
 
-    const user = await User.findById(req.params.id).exec();
-    if (!user) return res.status(404).send({ message: "No se encontró el usuario" });
-
-    const existsByNickname = await User.findOne({ nickname: req.body.nickname }).exec();
-    if (existsByNickname && existsByNickname._id !== req.params.id) 
-      return res.status(409).send({ message: "Ya existe un usuario con ese nickname" });
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      const error = new Error();
+      error.status = 404;
+      error.message = 'No se encontró el usuario';
+      throw error;
     }
     
     if (req.body.password) {
       // validar formato correccto de password
-      const { errors, isPasswordValid } = await validatePassword(req.body);
-      if (!isPasswordValid) return res.status(400).json(errors);
+      const isPasswordValid = await validatePassword(req.body);
+      if (!isPasswordValid) {
+        const error = new Error();
+        error.status = 404;
+        throw error;
+      }
 
       // validar que la nueva no sea igual a la anterior
-      const user = await User.findById(req.params.id).exec();
       const isPasswordMatch = await bcrypt.compare(req.body.password, user.password);
       if (isPasswordMatch) {
-          return res.status(400).json({ message: 'La nueva contraseña debe ser diferente a la actual' });
-        }
+        const error = new Error();
+        error.status = 404;
+        error.message = 'La nueva contraseña debe ser diferente a la actual';
+        throw error;
+      }
     }
 
       // actualizar usuario
-    const updatedUser = await User.findByIdAndUpdate (req.params.id, req.body, { new: true }).exec();
-    return res.status(200).send(updatedUser).json({ message: 'Usuario actualizado'});;
-  } catch (error) {
-    return res.status(500).send({ message: "Error al actualizar el usuario" });
-  }
+    const updatedUser = await User.findByIdAndUpdate (req.params.id, req.body, { new: true });
+    return res.status(200).send(updatedUser);
 };
 
 const DeleteUser = async (req, res) => {
-  try {
-    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-      return res.status(400).json({ message: 'ID inválido' });
-      }
+  //  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+  //    return res.status(400).json({ message: 'ID inválido' });
+
       const deletedUser = await User.findByIdAndDelete(req.params.id).exec();
       if (!deletedUser) return res.status(404).json({ message: 'No se encontró el usuario' });
       return res.status(200).json({ message: 'Usuario eliminado', deletedUser });
-  }catch (error) {
-    return res.status(500).json({ message: "Error al eliminar el usuario", error });
-    }
+
 };
 
 module.exports = { AddUser, FindAllUsers , FindSingleUser, UpdateUser, DeleteUser };
