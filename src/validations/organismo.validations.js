7f36527ecc7060eb9425618ef023502af99c1d81
Yoan Estrada Blanco <yoanestrada01@gmail.com>
2023-03-08 22:11:57 -0500

    refactor: :fire: refactor al DTO organismo
    Se refactorio la validacion del organismo

--- src/validations/organismo.validations.js
+++ src/validations/organismo.validations.js
@@ -1,9 +1,8 @@
-// const isEmpty = require('./isEmpty');
-// const validator = require('validator');
-// const { Type } = require('@sinclair/typebox');
-// const addErrors = require('ajv-errors');
-// const Ajv = require('ajv');
-// const ajv = new Ajv({ allErrors: true }).addKeyword('kind').addKeyword('modifier');
+const { Type } = require('@sinclair/typebox');
+const addErrors = require('ajv-errors');
+const addFormats = require('ajv-formats');
+const Ajv = require('ajv');
+const ajv = new Ajv({ allErrors: true });
 
 // /* ------------- EMPTY FIELD VALIDATION 4 REQUIRED */
 // const EmptyFieldOrganismo = (data) => {
@@ -24,50 +23,64 @@
 //   };
 // };
 
-/* ------------- AJV TYPE VALIDATION */
-// const OrganismoValidationSchema = Type.Object({
-//   name: Type.String({
-//     minLength: 2,
-//     maxLength: 30,
-//     errorMessage: {
-//       type: 'El tipo no es válido, debe ser string',
-//       minLength: 'debe tener minimo 2 caracteres',
-//       maxLength: 'debe tener máximo 30 caracteres',
-//     },
-//   }),
-//   description: Type.String({
-//     minLength: 10,
-//     maxLength: 80,
-//     errorMessage: {
-//       type: 'El tipo no es válido, debe ser string',
-//       minLength: 'debe tener minimo 10 caracteres',
-//       maxLength: 'debe tener máximo 80 caracteres',
-//     },
-//   }),
-//   priorizado: Type.Boolean({
-//     errorMessage: { type: 'El tipo de priorizado no es válido, debe ser boolean' },
-//   }),
-// });
+/**
+ * @return AJV JsonSchema
+ */
+const OrganismoValidationSchema = Type.Object(
+  {
+    name: Type.String({
+      minLength: 2,
+      maxLength: 30,
+      errorMessage: {
+        type: 'El tipo no es válido, debe ser string',
+        minLength: 'debe tener minimo 2 caracteres',
+        maxLength: 'debe tener máximo 30 caracteres',
+      },
+    }),
+    description: Type.String({
+      minLength: 10,
+      maxLength: 80,
+      errorMessage: {
+        type: 'El tipo no es válido, debe ser string',
+        minLength: 'debe tener minimo 10 caracteres',
+        maxLength: 'debe tener máximo 80 caracteres',
+      },
+    }),
+    priorizado: Type.Boolean({
+      errorMessage: { type: 'El tipo de priorizado no es válido, debe ser boolean' },
+    }),
+    weight: Type.Number({
+      errorMessage: { type: 'El tipo de peso no es válido, debe ser un número' },
+    }),
+  },
+  {
+    additionalProperties: false,
+    errorMessage: {
+      additionalProperties: 'Estas enviando data adicionales',
+    },
+  }
+);
 
+addFormats(ajv).addKeyword('kind').addKeyword('modifier');
 addErrors(ajv);
 
 const validateSchema = ajv.compile(OrganismoValidationSchema);
 
-// const organismoDataValidation = (req, res, next) => {
-//   const isEmpty = EmptyFieldOrganismo();
-//   const isDataValid = validateSchema(req.body);
+const organismoDataValidation = (req, res, next) => {
+  // const isEmpty = EmptyFieldOrganismo();
+  const isDataValid = validateSchema(req.body);
 
-//   if (isEmpty)
-//     return res.status(400).send({
-//       errors: EmptyFieldOrganismo.errors.map((error) => error.message),
-//     });
+  // if (isEmpty)
+  //   return res.status(400).send({
+  //     errors: EmptyFieldOrganismo.errors.map((error) => error.message),
+  //   });
 
-//   if (!isDataValid)
-//     return res.status(400).send({
-//       errors: validateSchema.errors.map((error) => error.message),
-//     });
+  if (!isDataValid)
+    return res.status(400).send({
+      errors: validateSchema.errors.map((error) => error.message),
+    });
 
-//   next();
-// };
+  next();
+};
 
 module.exports = { organismoDataValidation };
