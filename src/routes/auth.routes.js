84e1e326a316dd9be0c8ae054541dff8bb507ebc
Yoan Estrada Blanco <yoanestrada01@gmail.com>
2023-03-13 18:42:14 -0400

    feat: se creo las rutas del auth
    Se creo las rutas de auth y se implemnto el sigin

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const UserController = require('../controllers/auth.controller');
+
+router.post('/sigin', UserController.sigIn);
+
+router.post('/another-route', (req, res) => {
+  // router code here
+});
+
+module.exports = router;
