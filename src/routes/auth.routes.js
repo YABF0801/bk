b91ec9eb2c10c947da7e31e25af34912fd59f9d3
Yoan Estrada Blanco <yoanestrada01@gmail.com>
2023-03-13 19:16:04 -0400

    refactor: se elimino una ruta
    se elimino una ruta inutilisable del auth routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,8 +3,4 @@ const UserController = require('../controllers/auth.controller');
 
 router.post('/signin', UserController.sigIn);
 
-router.post('/another-route', (req, res) => {
-  // router code here
-});
-
 module.exports = router;
