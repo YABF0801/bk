f22b047af9804587e0c7d6aec0598f727705e5af
YABF0801 <yabf0801@gmail.com>
2023-03-17 19:11:42 -0400

    refactor: comentar midlewares
    comentar isAdmin isAuthorized en la ruta de circulo

--- src/routes/circulo.routes.js
+++ src/routes/circulo.routes.js
@@ -6,8 +6,8 @@ const {
   UpdateCirculo,
   DeleteCirculo,
 } = require('../controllers/circulo/circulo.controller');
-const isAdmin = require('../middlewares/isAdmin');
-const isAuthorized = require('../middlewares/isAuthorized');
+/* const isAdmin = require('../middlewares/isAdmin');
+const isAuthorized = require('../middlewares/isAuthorized'); */
 const { circuloDataValidation } = require('../validations/circulo.validations');
 /* const isAdmin = require('../midlewares/isAdmin');
 const isAuthorized = require('../midlewares/isAuthorized'); */
@@ -15,7 +15,7 @@ const isAuthorized = require('../midlewares/isAuthorized'); */
 const circuloRouter = Router();
 
 circuloRouter.post('/', /* isAuthorized, isAdmin, */ [circuloDataValidation], AddCirculo);
-circuloRouter.get('/', [isAuthorized, isAdmin], FindAllCirculos);
+circuloRouter.get('/', /* [isAuthorized, isAdmin], */ FindAllCirculos);
 circuloRouter.get('/:id', /* isAuthorized, isAdmin, */ FindSingleCirculo);
 circuloRouter.put('/:id', /* isAuthorized, isAdmin, */ [circuloDataValidation], UpdateCirculo);
 circuloRouter.delete('/:id', /* isAuthorized, isAdmin, */ DeleteCirculo);
