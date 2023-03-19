844325544b0aa6d5def37259e402b490b88bd821
YABF0801 <yabf0801@gmail.com>
2023-03-19 10:48:47 -0400

    fix: fix submision validations update
    fix validations por entrynumber/año y por carnet para la actualizacion de submision

--- src/controllers/submision/submision.controller.js
+++ src/controllers/submision/submision.controller.js
@@ -91,6 +91,33 @@ const UpdateSubmision = async (req, res) => {
         error.message = 'No se encontró la planilla';
         throw error;
       }
+      
+      if (submision.entryNumber !== req.body.entryNumber) {
+        // Verificar que no exista una submision con el mismo número y el mismo año de creación
+        const now = new Date(); // fecha actual 2023
+        const submisionExist = await Submision.findOne({ 
+          entryNumber: req.body.entryNumber, 
+          createdAt: { $gte: now.getFullYear(), $lte: now } // operador de comparacion de mongo greater than or equal >=, lte <=
+        });
+        if (submisionExist) {
+          const error = new Error();
+          error.status = 409;
+          error.message = 'Error al guardar la planilla, ya existe una submisión con el mismo número y año de creación';
+          throw error;
+        }
+      }
+
+      if (submision.child.carnet !== req.body.child.carnet) {
+        // Validar que no exista un niño con el mismo numero de carnet 
+          const carnetExist = await Submision.findOne({ 'child.carnet' : req.body.child.carnet});
+          if (carnetExist) {
+            const error = new Error();
+            error.status = 409;
+            error.message = 'Error al guardar la planilla, ya existe un niño con ese carnet';
+            throw error;
+          } 
+        }
+        
           const updatedSubmision = await Submision.findByIdAndUpdate(req.params.id, req.body, { new: true });
           return res.status(200).send(updatedSubmision);
    };
