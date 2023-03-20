77020a57fcd039b043b3964b7417243eeacd2ab2
YABF0801 <yabf0801@gmail.com>
2023-03-19 21:15:18 -0400

    update circulos routes

--- src/routes/circulo.routes.js
+++ src/routes/circulo.routes.js
@@ -1,23 +1,15 @@
 const { Router } = require('express');
-const {
-  AddCirculo,
-  FindAllCirculos,
-  FindSingleCirculo,
-  UpdateCirculo,
-  DeleteCirculo,
-} = require('../controllers/circulo/circulo.controller');
-/* const isAdmin = require('../middlewares/isAdmin');
-const isAuthorized = require('../middlewares/isAuthorized'); */
+const CirculoController = require('../controllers/circulo/circulo.controller');
 const { circuloDataValidation } = require('../validations/circulo.validations');
 /* const isAdmin = require('../midlewares/isAdmin');
 const isAuthorized = require('../midlewares/isAuthorized'); */
 
 const circuloRouter = Router();
 
-circuloRouter.post('/', /* isAuthorized, isAdmin, */ [circuloDataValidation], AddCirculo);
-circuloRouter.get('/', /* [isAuthorized, isAdmin], */ FindAllCirculos);
-circuloRouter.get('/:id', /* isAuthorized, isAdmin, */ FindSingleCirculo);
-circuloRouter.put('/:id', /* isAuthorized, isAdmin, */ [circuloDataValidation], UpdateCirculo);
-circuloRouter.delete('/:id', /* isAuthorized, isAdmin, */ DeleteCirculo);
+circuloRouter.post('/', /* isAuthorized, isAdmin, */ [circuloDataValidation], CirculoController.AddCirculo);
+circuloRouter.get('/', /* [isAuthorized, isAdmin], */ CirculoController.FindAllCirculos);
+circuloRouter.get('/:id', /* isAuthorized, isAdmin, */ CirculoController.FindSingleCirculo);
+circuloRouter.put('/:id', /* isAuthorized, isAdmin, */ [circuloDataValidation], CirculoController.UpdateCirculo);
+circuloRouter.delete('/:id', /* isAuthorized, isAdmin, */ CirculoController.DeleteCirculo);
 
 module.exports = circuloRouter;
