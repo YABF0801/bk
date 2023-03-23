c5f778b5a990bdcde1df020368b40655ec48155a
YABF0801 <yabf0801@gmail.com>
2023-03-23 11:06:36 -0500

    refactor: funcion Aceptar propuestas
    reestructurar, agregar validaciones y operadores de mongo a la funicon aceptar propuestas

--- src/services/manageMatriculas.js
+++ src/services/manageMatriculas.js
@@ -1,20 +1,57 @@
-const { mongoose } = require("mongoose");
 const Submision = require("../schemas/submision.schema");
+const Circulo = require("../schemas/circulo.schema"); 
 
+/* const AceptarPropuesta = require('../services/aceptarPropuesta'); */
+/* router.post('/aceptarPropuesta', AceptarPropuesta);  */
 
 const AceptarPropuesta = async (req, res) => {
-  try {
-    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-      return res.status(400).json({ message: 'ID inválido' });
-    }
-    const submision = await Submision.findById(req.params.id).exec();
-    submision.status = 'matricula';
-    await submision.save();
+  const { aprobadas } = req.body; // obetener el arreglo de las sumisions aprobadas que se envia desde el frontend
+  if (!Array.isArray(aprobadas) || aprobadas.length === 0) {
+    return res.status(400).json({ message: 'el arreglo rechazadas no es correcto o esta vacio' });
+  }
 
-    } catch (error) {
-      res.status(500).json({ message: 'Error al procesar los datos', error });
+  const now = new Date();
+
+  try {
+      // Obtener las submisions del arreglo de propuestas aceptadas
+      const submisionsAprobadas = await Submision.find({_id: { $in: aprobadas, status: 'propuesta' }}).populate('child.circulo');
+
+      for (const submisionAprobada of submisionsAprobadas) {
+        if (!submisionAprobada) {
+          const error = new Error();
+          error.status = 404;
+          error.message = 'No se encontró la planilla para matricular';
+          throw error;
+          }
+
+      const circulo = submisionAprobada.child.circulo;
+        if (!circulo) {
+          const error = new Error();
+          error.status = 404;
+          error.message = 'No se encontró el circulo propuesto en la planilla';
+          throw error;
+          }
+
+      const yearOfLife = submisionAprobada.child.year_of_life;
+      const sex = submisionAprobada.child.sex;
+      if (sex === 'femenino') {
+        await Circulo.updateOne({ _id: circulo._id }, { $inc: { [`matricula${yearOfLife}`]: 1, [`girls${yearOfLife}`]: 1 }});
+      } else {
+        await Circulo.updateOne({ _id: circulo._id }, { $inc: { [`matricula${yearOfLife}`]: 1 }});
+      }
+
+    await submisionAprobada.updateOne({$set: { status: 'matricula', matriculaDate: now },
+      });
     }
+
+    res.status(200).json({ message: 'Propuestas aceptadas matriculadas con exito' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error al procesar las propuestas' });
+  }
 };
+
+
 
 const RechazarPropuesta = async (req, res) => {
   try {
