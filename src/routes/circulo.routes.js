efd24be880e5cea3ebc1fbead2e1687e994a6edf
YABF0801 <yabf0801@gmail.com>
2023-03-24 20:40:52 -0400

    refactor: :art: añadir rutas circulo
    añadir rutas para funciones de ProyectarMatriculas y CambioDeCurso

--- src/routes/circulo.routes.js
+++ src/routes/circulo.routes.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const CirculoController = require('../controllers/circulo/circulo.controller');
 const { circuloDataValidation } = require('../validations/circulo.validations');
+const CirculoUtiles = require('../services/Utiles');
 /* const isAdmin = require('../midlewares/isAdmin');
 const isAuthorized = require('../midlewares/isAuthorized'); */
 
@@ -11,5 +12,8 @@ circuloRouter.get('/', /* [isAuthorized, isAdmin], */ CirculoController.FindAllC
 circuloRouter.get('/:id', /* isAuthorized, isAdmin, */ CirculoController.FindSingleCirculo);
 circuloRouter.put('/:id', /* isAuthorized, isAdmin, */ [circuloDataValidation], CirculoController.UpdateCirculo);
 circuloRouter.delete('/:id', /* isAuthorized, isAdmin, */ CirculoController.DeleteCirculo);
+
+circuloRouter.get('/', /* isAuthorized, */ CirculoUtiles.ProyectarMatriculas);
+circuloRouter.post('/', /* isAuthorized, , isAdmin, */ CirculoUtiles.CambioDeCurso);
 
 module.exports = circuloRouter;
