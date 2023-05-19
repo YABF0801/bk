b857fc43e378491b41dff851b42dcc88e694d37b
Yoan Estrada Blanco <45434999+yoan0102@users.noreply.github.com>
2023-05-19 14:18:36 -0400

    fix: se protegio las rutas del users

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
 const UserController = require('../controllers/user/user.controller');
-const {userDataValidation} = require('../validations/user.validations');
+const { userDataValidation } = require('../validations/user.validations');
 /* const { Login, Logout } = require('../controllers/user/auth.controller'); */
-/* const isAdmin = require('../midlewares/isAdmin');  */
+const isAdmin = require('../middlewares/isAdmin');
+const isAuthorized = require('../middlewares/isAuthorized');
 
 const userRouter = Router();
 
@@ -11,10 +12,10 @@ const userRouter = Router();
 userRouter.get('/logout', Logout);  */
 
 // otras rutas de user
-userRouter.post('/', /* isAuthorized, isAdmin, */ [userDataValidation], UserController.AddUser);
-userRouter.get('/', /* isAuthorized, isAdmin,  */ UserController.FindAllUsers);
-userRouter.get('/:id', /* isAuthorized, isAdmin, */ UserController.FindSingleUser);
-userRouter.put('/:id', /* isAuthorized,  isAdmin, */ [userDataValidation], UserController.UpdateUser);
-userRouter.delete('/:id', /* isAuthorized,  isAdmin,  */ UserController.DeleteUser);
+userRouter.post('/', isAuthorized, isAdmin, [userDataValidation], UserController.AddUser);
+userRouter.get('/', isAuthorized, isAdmin, UserController.FindAllUsers);
+userRouter.get('/:id', isAuthorized, isAdmin, UserController.FindSingleUser);
+userRouter.put('/:id', isAuthorized, isAdmin, [userDataValidation], UserController.UpdateUser);
+userRouter.delete('/:id', isAuthorized, isAdmin, UserController.DeleteUser);
 
 module.exports = userRouter;
