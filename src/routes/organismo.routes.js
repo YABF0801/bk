c36e397213a186c2095809d6abc17dc74a9159f1
Yoan Estrada Blanco <45434999+yoan0102@users.noreply.github.com>
2023-05-19 14:46:15 -0400

    fix: se protegio rutas de organismos

--- src/routes/organismo.routes.js
+++ src/routes/organismo.routes.js
@@ -2,15 +2,15 @@ const Router = require('express');
 const OrganismoController = require('../controllers/organismo/organismo.controller');
 
 const { organismoDataValidation } = require('../validations/organismo.validations');
-/* const isAdmin = require('../midlewares/isAdmin');
-const isAuthorized = require('../midlewares/isAuthorized'); */
+const isAdmin = require('../middlewares/isAdmin');
+const isAuthorized = require('../middlewares/isAuthorized');
 
 const organismoRouter = Router();
 
-organismoRouter.post('/', [organismoDataValidation], /* isAuthorized, isAdmin, */ OrganismoController.AddOrganismo);
-organismoRouter.get('/', /* isAuthorized, */ OrganismoController.FindAllOrganismos);
-organismoRouter.get('/:id', /* isAuthorized, */ OrganismoController.FindSingleOrganismo);
-organismoRouter.put('/:id', [organismoDataValidation], /* isAuthorized, isAdmin, */ OrganismoController.UpdateOrganismo);
-organismoRouter.delete('/:id', /* isAuthorized, isAdmin, */ OrganismoController.DeleteOrganismo);
+organismoRouter.post('/', [organismoDataValidation], isAuthorized, isAdmin, OrganismoController.AddOrganismo);
+organismoRouter.get('/', isAuthorized, OrganismoController.FindAllOrganismos);
+organismoRouter.get('/:id', isAuthorized, OrganismoController.FindSingleOrganismo);
+organismoRouter.put('/:id', [organismoDataValidation], isAuthorized, isAdmin, OrganismoController.UpdateOrganismo);
+organismoRouter.delete('/:id', isAuthorized, isAdmin, OrganismoController.DeleteOrganismo);
 
 module.exports = organismoRouter;
