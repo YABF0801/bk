afb1c1d000418c7a32c556435b9b534dab3a2370
Yoan Estrada Blanco <45434999+yoan0102@users.noreply.github.com>
2023-05-19 16:03:33 -0400

    fix: se protejuioo la rutas de submisison managaMatricula

--- src/routes/submision.routes.js
+++ src/routes/submision.routes.js
@@ -4,23 +4,29 @@ const SubmisionController = require('../controllers/submision/submision.controll
 const SubmisionUtiles = require('../services/Utiles');
 const ManageMatriculas = require('../services/manageMatriculas');
 const { submisionDataValidation } = require('../validations/submision.validations');
-/* const isAdmin = require('../midlewares/isAdmin');
-const isAuthorized = require('../midlewares/isAuthorized'); */
+const isAdmin = require('../middlewares/isAdmin');
+const isAuthorized = require('../middlewares/isAuthorized');
 
 const submisionRouter = Router();
+
+// TODO: hacer pruebas
 // NUMERO CONSECUTIVO 
-submisionRouter.put('/reset-consecutive', /* isAuthorized,  isAdmin, */ SubmisionUtiles.ResetConsecutive); /* Resetear el numero de entrada consecutivo */
+submisionRouter.put('/reset-consecutive', isAuthorized, isAdmin, SubmisionUtiles.ResetConsecutive); /* Resetear el numero de entrada consecutivo */
 submisionRouter.get('/get-tools', /* isAuthorized,  isAdmin, */ SubmisionUtiles.GetTools); /* Obtener el valoor aactual del consecutivo */
+//
+
 
 // CRUD
-submisionRouter.post('/', /* isAuthorized, isAdmin, */ [submisionDataValidation], SubmisionController.AddSubmision);
-submisionRouter.get('/', /* isAuthorized, */ SubmisionController.FindAllSubmisions);
-submisionRouter.get('/:id', /* isAuthorized,  isAdmin, */ SubmisionController.FindSingleSubmision); 
-submisionRouter.put('/:id', /* isAuthorized,  isAdmin, */ [submisionDataValidation], SubmisionController.UpdateSubmision);
-submisionRouter.delete('/:id', /* isAuthorized,  isAdmin, */ SubmisionController.DeleteSubmision);
+submisionRouter.post('/', isAuthorized, isAdmin, [submisionDataValidation], SubmisionController.AddSubmision);
+submisionRouter.get('/', isAuthorized, SubmisionController.FindAllSubmisions);
+submisionRouter.get('/:id', isAuthorized, isAdmin, SubmisionController.FindSingleSubmision);
+submisionRouter.put('/:id', isAuthorized, isAdmin, [submisionDataValidation], SubmisionController.UpdateSubmision);
+submisionRouter.delete('/:id', isAuthorized, isAdmin, SubmisionController.DeleteSubmision);
 
+
+// TODO: hacer pruebas
 // MANAGE MATRICULA
-submisionRouter.put('/matricular/:id', /* isAuthorized,  isAdmin, */ ManageMatriculas.MatriculaManual); /* hacer la matricula manual */
+submisionRouter.put('/matricular/:id', isAuthorized, isAdmin, ManageMatriculas.MatriculaManual); /* hacer la matricula manual */
 submisionRouter.put('/baja/:id', /* isAuthorized,  isAdmin, */ ManageMatriculas.Baja); /* dar baja de la matricula */
 
 
