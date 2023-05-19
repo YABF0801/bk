39199369ddb03c310611095304f87e0f2ee31118
Yoan Estrada Blanco <45434999+yoan0102@users.noreply.github.com>
2023-05-19 14:38:20 -0400

    fix: basico de circulos protegidos las rutas

--- src/routes/circulo.routes.js
+++ src/routes/circulo.routes.js
@@ -2,23 +2,26 @@ const { Router } = require('express');
 const CirculoController = require('../controllers/circulo/circulo.controller');
 const CirculoUtiles = require('../services/Utiles');
 const { circuloDataValidation } = require('../validations/circulo.validations');
-/* const isAdmin = require('../midlewares/isAdmin');
-const isAuthorized = require('../midlewares/isAuthorized'); */
+const isAdmin = require('../middlewares/isAdmin');
+const isAuthorized = require('../middlewares/isAuthorized');
 
 const circuloRouter = Router();
 
-circuloRouter.get('/proyectar', /* isAuthorized, */ CirculoUtiles.ProyectarMatriculas);
-circuloRouter.get('/crear-copia', /* isAuthorized, */ CirculoUtiles.CirculosCopia);
-circuloRouter.post('/set-curso', /* isAuthorized, , isAdmin, */ CirculoUtiles.AddCurso);
-circuloRouter.post('/nuevo-curso', /* isAuthorized, , isAdmin, */ CirculoUtiles.NewCurso);
 
-circuloRouter.post('/', /* isAuthorized, isAdmin, */ [circuloDataValidation], CirculoController.AddCirculo);
-circuloRouter.get('/', /* [isAuthorized, isAdmin], */ CirculoController.FindAllCirculos);
-circuloRouter.get('/:id', /* isAuthorized, isAdmin, */ CirculoController.FindSingleCirculo);
-circuloRouter.put('/:id', /* isAuthorized, isAdmin, */ [circuloDataValidation], CirculoController.UpdateCirculo);
-circuloRouter.delete('/:id', /* isAuthorized, isAdmin, */ CirculoController.DeleteCirculo);
+// TODO: Hacer pruebas
+circuloRouter.get('/proyectar', isAuthorized, CirculoUtiles.ProyectarMatriculas);
+circuloRouter.get('/crear-copia', isAuthorized, CirculoUtiles.CirculosCopia);
+circuloRouter.post('/set-curso', isAuthorized, isAdmin, CirculoUtiles.AddCurso);
+circuloRouter.post('/nuevo-curso', isAuthorized, isAdmin, CirculoUtiles.NewCurso);
+//
 
-circuloRouter.put('/status/:id',  /* isAuthorized, , isAdmin, */ CirculoUtiles.DeactivateCirculo);
+circuloRouter.post('/', isAuthorized, isAdmin, [circuloDataValidation], CirculoController.AddCirculo);
+circuloRouter.get('/', [isAuthorized, isAdmin], CirculoController.FindAllCirculos);
+circuloRouter.get('/:id', isAuthorized, isAdmin, CirculoController.FindSingleCirculo);
+circuloRouter.put('/:id', isAuthorized, isAdmin, [circuloDataValidation], CirculoController.UpdateCirculo);
+circuloRouter.delete('/:id', isAuthorized, isAdmin, CirculoController.DeleteCirculo);
+
+circuloRouter.put('/status/:id', isAuthorized, isAdmin, CirculoUtiles.DeactivateCirculo);
 
 module.exports = circuloRouter;
 
