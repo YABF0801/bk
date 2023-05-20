31291af6110f9c2b0b6fda6385365091ef13f6e4
YABF0801 <yabf0801@gmail.com>
2023-05-19 23:34:51 -0400

    protegidas rutas de propuesttas

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,17 +1,11 @@
 const { Router } = require('express');
 const UserController = require('../controllers/user/user.controller');
 const { userDataValidation } = require('../validations/user.validations');
-/* const { Login, Logout } = require('../controllers/user/auth.controller'); */
 const isAdmin = require('../middlewares/isAdmin');
 const isAuthorized = require('../middlewares/isAuthorized');
 
 const userRouter = Router();
 
-// rutas de sesion
-/* userRouter.post('/login', Login);
-userRouter.get('/logout', Logout);  */
-
-// otras rutas de user
 userRouter.post('/', isAuthorized, isAdmin, [userDataValidation], UserController.AddUser);
 userRouter.get('/', isAuthorized, isAdmin, UserController.FindAllUsers);
 userRouter.get('/:id', isAuthorized, isAdmin, UserController.FindSingleUser);
